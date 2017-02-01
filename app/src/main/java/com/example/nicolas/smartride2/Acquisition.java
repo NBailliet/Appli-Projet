4903357adb494ce71bb004c8ab958a4c636ce222
Nicolas <Nicolas@10-238-34-9.aerius.USherbrooke.ca>
2017-02-01 13:26:40 -0500

    initial commit

---
+++ app/src/main/java/com/example/nicolas/smartride2/Acquisition.java
@@ -0,0 +1,24 @@
+package com.example.nicolas.smartride2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nicolas on 30/01/2017.
+ */
+
+public class Acquisition extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acquisition);
+    }
+}
