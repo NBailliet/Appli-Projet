5a0696f367c3ddf3a997c16567a89424d281b530
Nicolas <Nicolas@10-238-34-48.aerius.USherbrooke.ca>
2017-02-02 10:49:49 -0500

    all changes

---
+++ app/src/main/java/com/example/nicolas/smartride2/ShareFragment.java
@@ -0,0 +1,20 @@
+package com.example.nicolas.smartride2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nicolas on 01/02/2017.
+ */
+
+public class ShareFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View shareView = inflater.inflate(R.layout.share, container, false);
+        return shareView;
+    }
+}
--- app/src/main/java/com/example/nicolas/smartride2/SmartRide.java
+++ app/src/main/java/com/example/nicolas/smartride2/SmartRide.java
@@ -1,23 +1,17 @@
 package com.example.nicolas.smartride2;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.MenuInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.RadioButton;
+
 
 public class SmartRide extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,14 +24,6 @@ public class SmartRide extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -47,6 +33,8 @@ public class SmartRide extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        onNavigationItemSelected(navigationView.getMenu().findItem(R.id.home));
+
     }
 
     @Override
@@ -70,15 +58,25 @@ public class SmartRide extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the HomeFragment/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        FragmentManager fm = getSupportFragmentManager();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            fm.beginTransaction().replace(R.id.frame,new SettingsFragment()).commit();
+            setTitle(getString(R.string.action_settings));
             return true;
         }
         else if (id == R.id.action_bluetooth) {
+            fm.beginTransaction().replace(R.id.frame,new BluetoothFragment()).commit();
+            setTitle(getString(R.string.action_bluetooth));
+            return true;
+        }
+        else if (id == R.id.action_profile) {
+            fm.beginTransaction().replace(R.id.frame,new ProfileFragment()).commit();
+            setTitle(getString(R.string.action_profile));
             return true;
         }
         else if (id == R.id.action_quitter) {
@@ -95,20 +93,26 @@ public class SmartRide extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fm = getSupportFragmentManager();
 
-        if (id == R.id.acquisition) {
-            Button butAcquisition = (Button) findViewById(R.id.acquisition);
-            // Handle the camera action
+        if (id == R.id.home) {
+            fm.beginTransaction().replace(R.id.frame,new HomeFragment()).commit();
+            setTitle(getString(R.string.action_home));
+        } else if (id == R.id.record) {
+            fm.beginTransaction().replace(R.id.frame,new RecordFragment()).commit();
+            setTitle(getString(R.string.action_record));
         } else if (id == R.id.overview) {
-
-        } else if (id == R.id.gps) {
-
-        } else if (id == R.id.nav_manage) {
-
+            fm.beginTransaction().replace(R.id.frame,new OverviewFragment()).commit();
+            setTitle(getString(R.string.action_overview));
+        } else if (id == R.id.map) {
+            fm.beginTransaction().replace(R.id.frame,new MapFragment()).commit();
+            setTitle(getString(R.string.action_map));
         } else if (id == R.id.nav_share) {
-
+            fm.beginTransaction().replace(R.id.frame,new ShareFragment()).commit();
+            setTitle(getString(R.string.action_share));
         } else if (id == R.id.nav_send) {
-
+            fm.beginTransaction().replace(R.id.frame,new SendFragment()).commit();
+            setTitle(getString(R.string.action_send));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
