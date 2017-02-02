c2734e8d7845c693fb1f3f189531072a5efc8705
valente007 <valentin.limantour@gmail.com>
2017-02-01 23:01:21 -0500

    add action bluetooth

--- app/src/main/java/com/example/nicolas/smartride2/SmartRide.java
+++ app/src/main/java/com/example/nicolas/smartride2/SmartRide.java
@@ -1,26 +1,43 @@
 package com.example.nicolas.smartride2;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.MenuInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class SmartRide extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int REQUEST_ENABLE_BT = 0;
+    private BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private ListView listView;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +63,11 @@ public class SmartRide extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+        registerReceiver(mReceiver, filter);
     }
 
     @Override
@@ -76,11 +98,22 @@ public class SmartRide extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        else if (id == R.id.action_bluetooth) {
+        } else if (id == R.id.action_bluetooth) {
+
+            if (mBluetoothAdapter == null) {
+                // Device does not support Bluetooth
+                Toast.makeText(SmartRide.this, "Your device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            }
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+
+
+
             return true;
-        }
-        else if (id == R.id.action_quitter) {
+        } else if (id == R.id.action_quitter) {
             finish();
             return true;
         }
@@ -88,6 +121,83 @@ public class SmartRide extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+
+                mBluetoothAdapter.startDiscovery();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(SmartRide.this);
+                // Add the buttons
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        mBluetoothAdapter.cancelDiscovery();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        mBluetoothAdapter.cancelDiscovery();
+                    }
+                });
+
+                builder.setIcon(R.drawable.bluetooth);
+
+                builder.setTitle("Bluetooth Devices");
+
+                LayoutInflater inflater = getLayoutInflater();
+                View convertView = (View) inflater.inflate(R.layout.bluetooth_device_list, null);
+                builder.setView(convertView);
+
+
+                // Create the AlertDialog
+                AlertDialog dialog = builder.create();
+
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                ArrayList<BluetoothDevice> list = new ArrayList<BluetoothDevice>();
+
+                list.addAll(pairedDevices);
+
+                listView = (ListView) convertView.findViewById(R.id.listView1);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mDeviceList);
+                listView.setAdapter(adapter);
+
+                //mBluetoothAdapter.startDiscovery();
+                Log.i("khvjfj","tag jhyjg");
+                dialog.show();
+
+                Toast.makeText(SmartRide.this, "Work !!", Toast.LENGTH_SHORT).show();
+            }
+
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(SmartRide.this, "Recording Data impossible without Bluetooth connection...", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            Log.i("BT","tag 1");
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                /*String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address*/
+                mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                Toast.makeText(SmartRide.this,"yo", Toast.LENGTH_SHORT).show();
+                listView.setAdapter(new ArrayAdapter<String>(context,
+                       android.R.layout.simple_list_item_1, mDeviceList));
+            }
+        }
+    };
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -113,5 +223,13 @@ public class SmartRide extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+   @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
     }
 }
